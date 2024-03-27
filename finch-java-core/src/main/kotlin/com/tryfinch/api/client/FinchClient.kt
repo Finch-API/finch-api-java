082c8f6e59705eb1005a5dc3298d7e5cd7b0a3c3
Robert Craigie <robert@craigie.dev>
2024-03-27 17:07:23 +0000

    chore: revert temporary commit

--- finch-java-core/src/main/kotlin/com/tryfinch/api/client/FinchClient.kt
+++ finch-java-core/src/main/kotlin/com/tryfinch/api/client/FinchClient.kt
@@ -4,6 +4,7 @@
 
 package com.tryfinch.api.client
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.tryfinch.api.models.*
 import com.tryfinch.api.services.blocking.*
 
@@ -26,4 +27,26 @@ interface FinchClient {
     fun jobs(): JobService
 
     fun sandbox(): SandboxService
+
+    fun getAccessToken(
+        clientId: String,
+        clientSecret: String,
+        code: String,
+        redirectUri: String
+    ): String
+
+    fun getAuthUrl(products: String, redirectUri: String, sandbox: Boolean): String
+
+    fun withAccessToken(accessToken: String): FinchClient
+
+    private data class GetAccessTokenParams(
+        @JsonProperty("client_id") val clientId: String,
+        @JsonProperty("client_secret") val clientSecret: String,
+        @JsonProperty("code") val code: String,
+        @JsonProperty("redirect_uri") val redirectUri: String,
+    )
+
+    private data class GetAccessTokenResponse(
+        @JsonProperty("accessToken") val accessToken: String,
+    )
 }
